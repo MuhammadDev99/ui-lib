437b944f12cb0af5bfdfdfd8621c1cbb60425426
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2026-06-08 15:39:13 +0300

    Update

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   dts: true,
   clean: true,
   splitting: false,
-  injectStyle: false, 
+  // Add this loader configuration to enable native CSS Modules compiling:
+  loader: {
+    '.css': 'local-css',
+  },
   external: ['react', 'react-dom', 'clsx', 'lucide-react'],
   banner: { js: '"use client";' },
 });
