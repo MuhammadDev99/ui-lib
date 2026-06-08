333e9f33622195fc39dfe288df152a724b7b5490
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2026-06-08 20:06:38 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@muhammad_dev_77/ui-lib': path.resolve(__dirname, './src/index.ts'),
-    },
-  },
   css: {
     modules: {
       localsConvention: 'camelCase',
